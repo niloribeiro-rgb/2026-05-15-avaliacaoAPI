e612dd130c7c5b1cb608a3677e2ebd9f4ba44707
Nilo Arthur <nilo.ribeiro@aluno.senai.br>
2026-05-15 17:27:18 -0300

    Fetch anime data using dynamic IDs from array

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 async function getAPI1() {
+    let codAnimes = [48583,52991,40748,20,57555]
+    for(let i=0;i<codAnimes.length;i++){
     try {
         const animeContainer = document.querySelector(`.animeContainer`)
 
-        const response = await fetch(`https://api.jikan.moe/v4/anime/40748`)
-        // https://api.jikan.moe/v4/anime/48583
-        // https://api.jikan.moe/v4/anime/52991
-        // https://api.jikan.moe/v4/anime/40748
-        // https://api.jikan.moe/v4/anime/20
-        // https://api.jikan.moe/v4/anime/57555
+        const response = await fetch(`https://api.jikan.moe/v4/anime/${codAnimes[i]}`)
+        // 48583
+        // 52991
+        // 40748
+        // 20
+        // 57555
         const dados = await response.json()
         if (!response.ok) {
             alert(`Erro: ${response.status} - ${response.statusText}`)
@@ -24,17 +26,20 @@ async function getAPI1() {
         console.log(videoKey)
         console.log(dados.data.trailer.embed_url)
 
-        
-        animeContainer.innerHTML = `<div class="animeCard">
+        // alert(dados.data.rating)
+        animeContainer.innerHTML += `<div class="animeCard">
         <img src="${dados.data.images.jpg.image_url}" alt="${dados.data.title}">
         <h2>${dados.data.title}</h2>
         <p>${dados.data.synopsis}</p>
         </div>
-        <h4>Traller</h4>
-        <iframe width="300" height="auto" src="https://www.youtube.com/embed/${videoKey}si=R-EVc6WSTISt_vc8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+        <p>${dados.data.rating}</p>
+        <iframe width="300" height="auto" src="https://www.youtube.com/embed/${videoKey}si=R-EVc6WSTISt_vc8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <hr>`
+        
 
     }
     catch (error) {
         console.error(error)
     }
+}
 }
