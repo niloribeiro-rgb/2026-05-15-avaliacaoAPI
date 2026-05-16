5161b8e8b150aeef40d31b3ea24a6f9ad20785dc
Nilo Arthur <nilo.ribeiro@aluno.senai.br>
2026-05-15 23:34:27 -0300

    Refactor anime API fetching and rendering logic

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+let codAnimes = [48583,52991,40748,57555,20]
 async function getAPI1() {
-    let codAnimes = [48583,52991,40748,20,57555]
-    for(let i=0;i<codAnimes.length;i++){
-    try {
-        const animeContainer = document.querySelector(`.animeContainer`)
+    
+    const animeContainer =             document.querySelector(`.animeContainer`)
 
+    animeContainer.innerHTML = ""
+    for(let i=0;i<=codAnimes.length;i++){
+    // alert(i)
+    try {
+        
         const response = await fetch(`https://api.jikan.moe/v4/anime/${codAnimes[i]}`)
         // 48583
         // 52991
@@ -12,13 +16,13 @@ async function getAPI1() {
         // 57555
         const dados = await response.json()
         if (!response.ok) {
-            alert(`Erro: ${response.status} - ${response.statusText}`)
+            alert(`Erro: ${response.status} - ${response.statusText} ${i}`)
         }
 
         let videoKey = ""
         if (dados.data.trailer.embed_url !== undefined) {
-            for (let i = 39; i < 51; i++) {
-                videoKey += dados.data.trailer.embed_url[i]
+            for (let c = 39; c < 51; c++) {
+                videoKey += dados.data.trailer.embed_url[c]
             }
             // for recorta da casa 39 a 51 do link recebido
             // da letra 39 ate 51 é padrao da chave principal do video 
@@ -27,16 +31,19 @@ async function getAPI1() {
         console.log(dados.data.trailer.embed_url)
 
         // alert(dados.data.rating)
-        animeContainer.innerHTML += `<div class="animeCard">
-        <img src="${dados.data.images.jpg.image_url}" alt="${dados.data.title}">
+        let cardConteudo = ""
+        cardConteudo += `<div class="animeCard" id="${i}">
+        <img src="${dados.data.images.webp.large_image_url}" alt="${dados.data.title}">
         <h2>${dados.data.title}</h2>
         <p>${dados.data.synopsis}</p>
-        </div>
-        <p>${dados.data.rating}</p>
-        <iframe width="300" height="auto" src="https://www.youtube.com/embed/${videoKey}si=R-EVc6WSTISt_vc8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <hr>`
         
-
+        <p>${dados.data.rating}</p>`
+        if (dados.data.trailer.embed_url !== undefined){
+            cardConteudo += ` <iframe width="300px" height="auto" src="https://www.youtube.com/embed/${videoKey}si=R-EVc6WSTISt_vc8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+        }
+       // alert(cardConteudo)
+        animeContainer.innerHTML += `${cardConteudo} <hr> ${i} </div>` 
+        
     }
     catch (error) {
         console.error(error)
